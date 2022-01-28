54a08375e6274c7925c48bade0a670f7a845b279
Noah <matanaworgen@gmail.com>
2022-01-28 14:26:29 -0500

    la fenetre s'ouvre :)

--- app/main.js
+++ app/main.js
@@ -1,45 +1,16 @@
 const { app, BrowserWindow } = require("electron");
-const path = require("path");
-const { Menu } = require("electron");
 
 function createWindow() {
-  // Pas de menu
-  // https://stackoverflow.com/a/66062765
-  Menu.setApplicationMenu(null);
-
   const win = new BrowserWindow({
-    show: false,
-    // Icône dans barre de titre et barre de tâche
-    // https://stackoverflow.com/a/65352577
-    icon: __dirname + "/decoration/illustration/logo-cadeau.png",
-    webPreferences: {
-      /*
-      preload: path.join(__dirname, 'preload.js')
-      */
-    },
+    width: 800,
+    height: 600,
   });
 
   win.loadFile("index.html");
-
-  // Affiche maximisé par défaut
-  // https://stackoverflow.com/a/45906784
-  win.maximize();
-  win.show();
-  win.webContents.openDevTools();
 }
 
-app.whenReady().then(() => {
-  createWindow();
+app.on("ready", createWindow);
 
-  app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
-});
-
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
 });
