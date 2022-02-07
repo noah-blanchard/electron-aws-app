3c91e99d3ed1d951e26fbb1db9db6aefcc466cd4
Noah Blanchard <n.blanchard190302@gmail.com>
2022-02-07 13:32:14 -0500

    app noah finie

--- app-noah/VueSoulier.js
+++ app-noah/VueSoulier.js
@@ -13,11 +13,11 @@ class VueSoulier{
       document.getElementById("soulier-nom").innerHTML = this.soulier.nom;
       document.getElementById("soulier-marque").innerHTML = this.soulier.marque;
       document.getElementById("soulier-description").innerHTML = this.soulier.description;
-      document.getElementById("soulier-couleur").innerHTML = this.soulier.couleur;
-      document.getElementById("soulier-materiaux").innerHTML = this.soulier.materiaux;
-      document.getElementById("soulier-pourQui").innerHTML = this.soulier.pourQui;
       document.getElementById("soulier-pointure").innerHTML = this.soulier.pointure;
+      document.getElementById("soulier-pourQui").innerHTML = this.soulier.pourQui;
       document.getElementById("soulier-fermeture").innerHTML = this.soulier.fermeture;
+      document.getElementById("soulier-materiaux").innerHTML = this.soulier.materiaux;
+      document.getElementById("soulier-couleur").innerHTML = this.soulier.couleur;
       document.getElementById("soulier-id").innerHTML = this.soulier.id;
     }
   
